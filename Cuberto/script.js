82a34f04ad86369b2948c850f1ecffddcef2a679
AyushGolwadiya <ayushgolvadiya@gmail.com>
2025-03-21 18:46:55 +0530

    Cuberto's site Landing page

---
+++ Cuberto/script.js
@@ -0,0 +1,10 @@
+Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration:1,
+});
+
+Shery.makeMagnet(".magnet");
+
+Shery.hoverWithMediaCircle(".hvr",{videos:["https://cuberto.com/assets/home/hero/header.mp4" , "https://cuberto.com/assets/showreel/short.mp4" , "https://cuberto.com/assets/featured/header.mp4"]})
+
